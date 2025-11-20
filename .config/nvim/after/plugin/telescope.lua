f6153f2914c8ecb02307d7d18d7c57e9902cd8b8
Tommy <thager09@gmail.com>
2025-11-20 14:26:54 -0500

    Added telescope config to make preview vertical

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,14 @@
+require('telescope').setup{
+  defaults = {
+    layout_strategy = 'vertical',
+    layout_config = {
+      prompt_position = 'bottom',
+      -- Optional: Adjust height/width as needed
+      -- height = 0.8,
+      -- width = 0.8
+    },
+  },
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
